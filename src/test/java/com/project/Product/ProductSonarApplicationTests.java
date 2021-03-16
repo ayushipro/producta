81f35ed21a3c58ddb6df0a5118bd237fa2ef1fb8
ayushi <ayushigupta4632@gmail.com>
2021-03-16 16:01:40 +0530

    a

--- src/test/java/com/project/Product/ProductSonarApplicationTests.java
+++ src/test/java/com/project/Product/ProductSonarApplicationTests.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-//import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
